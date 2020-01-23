55b806a493640234edb4dd73da2e2def2c8f75a4
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2020-01-23 02:56:14 +0000

    Part 2 Complete

--- exercises/part-2/index.js
+++ exercises/part-2/index.js
@@ -1,40 +1,27 @@
 // Get the node, assign to variable
-const name = document.getElementById('name');
-const container = document.getElementById('contact')
+const name = document.getElementById('name'); // Get P Tag
+const container = document.getElementById('contact') // Get Div Tag
 
-container.addEventListener('dblclick', function(e) {
-
-    const button = document.createElement("button"); // Create button
-    const user_input = document.createElement("input"); // Create input
-    user_input.setAttribute( "placeholder", "Type a new Name...");
-    user_input.setAttribute("id", "name") // Set ID
-    button.style.height = "18px"
+const button = document.createElement("button"); // Create button
+const user_input = document.createElement("input"); // Create input
+user_input.setAttribute( "placeholder", "Type a new Name..."); // Add placeholder to input
+button.style.height = "18px"; // Increase button size
+    
+name.addEventListener('dblclick', function(e) {
+    
     container.appendChild(user_input) // Append input tag to container
-    name.remove() // remove name tag
+    name.innerHTML = "" // remove name tag
     container.appendChild(button)
-    console.log(name)
-    // let temp = name.innerHTML
     
-    // 
-    // 
-    // 
-    // button.setAttribute("type", "submit")
-    // container.appendChild(button)
-    // newName.innerHTML = ''
-    // name.parentNode.replaceChild(newName, name)
-    
+
 });
 
+button.addEventListener("click", function(e) {
+    name.innerHTML = user_input.value;
+    button.remove()
+    user_input.remove()
+})
 
-    // // Create an empty element node
-    // 
-    
-    // // Give it an id attribute called 'name'
-    // // newName.id = "name";
-    // // // Set Value to value of current p tag.
-    // // newName.innerHTML = name.innerHTML
-
-// });
 
 // new event listener
 // find submit button
